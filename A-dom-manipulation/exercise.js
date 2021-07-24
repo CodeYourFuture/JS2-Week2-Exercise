00e65eb42ae256131f2713281cc6697d8d597eb7
Bimbola Lasisi <78049244+blasisi@users.noreply.github.com>
2021-07-24 18:37:05 +0100

    Class exercise completed
    class exercise completed

--- A-dom-manipulation/exercise.js
+++ A-dom-manipulation/exercise.js
@@ -1,9 +1,12 @@
+
 /*
 Task 1
 =======
 Write JavaScript below that logs:
     1. all the "p" element nodes of the document,
     --> should log a list of nodes with a length of 6
+    
+    let elementArray = Array.from(document.querySelectorAll("div"));
 
     2. the first div element node
     --> should log the ".site-header" node
@@ -16,6 +19,26 @@ Write JavaScript below that logs:
 
 */
 
+let paragraphEl  = document.querySelectorAll("p");
+console.log(paragraphEl);
+
+const elementArray = Array.from(paragraphEl);
+console.log(elementArray);
+
+const firstDiv =document.querySelector("div");
+console.log(firstDiv);
+
+const jumbotronText=document.querySelector("#jumbotronText");
+console.log(jumbotronText);
+
+// const primaryContent = document.querySelectorAll(".primary-content p");
+// console.log(primaryContent);
+
+const primaryContent = document.querySelector(".primary-content p")[1];
+console.log(primaryContent);
+
+
+
 
 /*
 Task 2
@@ -23,6 +46,26 @@ Task 2
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
+//  document
+//    .getElementById("alertBtn")
+//      .addEventListener("click", alertSomething);
+
+//      function alertSomething() {
+//         alert("hello CYF");
+//       }
+
+document.getElementById("alertBtn").addEventListener("click", alertMessage);
+function alertMessage() {
+    alert(" Alert CYF");
+}
+//       let alertButton = document.getElementById('alertBtn')
+
+
+// alertButton.addEventListener('click', ()=>{alert('Button was clicked')})
+
+// let myButton = document.querySelector("#myButton");
+// myButton.addEventListener("click", alertSomething);
+
 
 
 /*
@@ -32,13 +75,29 @@ Task 3
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
 
-
+const changeColor = document.querySelector("#bgrChangeBtn");
+changeColor.addEventListener("click", function(){
+    document.body.style.backgroundColor = "black";
+});
 /*
 Task 4
 ======
-
+const ourParagraph =document.createElemnt("p");
+ourParagraph
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
+
+const addTextButton = document.getElementById("addTextBtn")
+const addButtonPara = document.querySelector(".buttons")
+ addTextButton.addEventListener("click", () => {
+
+    const ourParagraph =document.createElement("p");
+    ourParagraph.innerText = "read more below" ;
+    addButtonPara.appendChild(ourParagraph);
+});
+
+
+
 
 
 
@@ -48,3 +107,11 @@ Task 5
 
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
+
+const largeLinkButton = document.getElementById("largerLinksBtn")
+ largeLinkButton.addEventListener("click", () => {
+ const links = document.querySelectorAll("a");
+ Array.from(links).forEach(link => {
+    link.style.fontSize = "3rem"
+ });
+});
